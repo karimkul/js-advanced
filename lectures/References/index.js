8d1a4085248cae8fc5666b10ff7b5a964f170529
Jami Karimkulov <jami.karimkul@gmail.com>
2024-07-29 22:56:28 -0400

    Add lecture references

---
+++ lectures/References/index.js
@@ -0,0 +1,26 @@
+// Problem Statement
+
+// Initialize original array and object
+let originalArray = [1, 2, 3];
+let originalObject = { key: "value" };
+
+// Reassign them to new variables
+let newArray = originalArray;
+let newObject = originalObject;
+
+// Make changes to the new variables
+newArray.push(4);
+newObject.newKey = "newValue";
+
+// Print the original and new variables
+console.log("originalArray:", originalArray);
+// Output: originalArray: [1, 2, 3, 4]
+
+console.log("newArray:", newArray);
+// Output: newArray: [1, 2, 3, 4]
+
+console.log("originalObject:", originalObject);
+// Output: originalObject: { key: 'value', newKey: 'newValue' }
+
+console.log("newObject:", newObject);
+// Output: newObject: { key: 'value', newKey: 'newValue' }
